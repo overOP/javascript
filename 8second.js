b84dfff9f0006548c654949bcb8d29f6f081b5e4
overOP <cgpradeepop@gmail.com>
2025-05-07 13:12:14 +0545

    fix the second

--- 8second.js
+++ 8second.js
@@ -1,17 +1,11 @@
-
 function countdown(start) {
     console.log(start);
     if (start <= 0) {
-        cleryInterval(intervalid);
+        clearInterval(intervalId);
         console.log("countdown finished");
-    }
-    else {
-        startValue--;
     }
 }
 let startValue = 10;
 //interval id sanding
 //setinterval
-const intervalid = setInterval(() => countdown(startValue), 1000);
-
-let 
+const intervalId = setInterval(() => countdown(startValue--), 1000);
